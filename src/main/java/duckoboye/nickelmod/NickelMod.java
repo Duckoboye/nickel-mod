484ca81f380ff03ddb0c7c1a3198c32d4ae84364
Duckoboye <swedishshadows@gmail.com>
2021-06-26 13:25:38 +0200

    Fixed typo that broke femboy sword texture.

--- src/main/java/duckoboye/nickelmod/NickelMod.java
+++ src/main/java/duckoboye/nickelmod/NickelMod.java
@@ -13,8 +13,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 // This is the mod id. It must be the same as the one in mods.toml.
 @Mod("nickelmod")
 public class NickelMod {
@@ -41,10 +43,8 @@ public class NickelMod {
         @SubscribeEvent
         public static void registerItems(final RegistryEvent.Register<Item> event) {
             event.getRegistry().registerAll(
-                    ItemList.nickel = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("nickel")),
-                    ItemList.femboy_sword = new SwordItem(ToolMaterialList.femboy_sword, 4, - 3, new
-                            Item.Properties().group(ItemGroup.COMBAT)).setRegistryName(location("femboy_sword"))
-
+                    ItemList.nickel = new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName(location("nickel")),
+                    ItemList.femboy_sword = new SwordItem(ToolMaterialList.femboy_sword, 4, - 3, new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName(location("femboy_sword"))
             );
         }
 
